45fbd2cf6dc22816f708dba7776c1bbdaebe6287
Jessy Hu <jesshuy12@gmail.com>
2019-02-06 03:54:55 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -13,12 +13,12 @@ end
 
 def get_japanese_emoticon(path, emoticon)             #two arguements, path and emoticon
   library = load_library(path)                        #calling load_library
-  library["get_emoticon"].each do |key, value|
-    if key == emoticon
-      return value
+  library["get_emoticon"].each do |key, value|        #iterate over library["get_emoticon"]
+    if key == emoticon                                #determine if key matches emoticon in arg
+      return value                                    #if match, return the japanese version "value"
     end
   end
-  return "Sorry, that emoticon was not found"
+  return "Sorry, that emoticon was not found"         #if not match
 end
 
 def get_english_meaning
