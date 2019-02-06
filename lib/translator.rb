cc31dc58813f9147fef9d9e0364a72d25845641a
Jessy Hu <jesshuy12@gmail.com>
2019-02-06 03:44:58 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -13,7 +13,8 @@ end
 
 def get_japanese_emoticon(path, emoticon)    #two arguements, path and emoticon
   library = load_library(path)               #calling load_library
-  ilbrary["get_emoticon"]
+  ilbrary["get_emoticon"].each do |key, value|
+    
 end
 
 def get_english_meaning
