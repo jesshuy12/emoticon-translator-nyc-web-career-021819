03eca0921e296b5b9f2b6232fb7f4e0bcb2e7ebd
Jessy Hu <jesshuy12@gmail.com>
2019-02-06 03:30:00 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,8 +5,8 @@ def load_library(path)
   emoticons = YAML.load_file(path)
   new_hash = {"get_meaning" => {}, "get_emoticon" => {}}    # return hash with two new keys, with                                                              inner hash
       emoticons.each do |key, value|
-        new_hash["get_emoticon"][value[1]] = key
-        new_hash["get_meaning"][value[0]] = value[1]
+        new_hash["get_meaning"][value[1]] = key
+        new_hash["get_emoticon"][value[0]] = value[1]
     end
     return new_hash
 end
