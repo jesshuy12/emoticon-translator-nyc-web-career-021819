232e0018a004e3d13b2cfade1b64700e60c1007c
Jessy Hu <jesshuy12@gmail.com>
2019-02-06 03:14:22 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,7 +5,8 @@ def load_library
   emoticons = YAML.load_file("emoticons.yml")
   new_hash = {"get_meaning" => {}, "get_emoticon" => {}}    # return hash with two new keys, with                                                              inner hash
     emoticons.each do |key, value|
-      english, japanese = array
+      english, japanese = value
+      
 end
 
 def get_japanese_emoticon
